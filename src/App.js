c40649b56dd9e4dd3441cad11422c969a39d696a
vishal <vishalchourasia7047@gmail.com>
2021-08-29 13:43:02 +0530

    signup task is completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Signup from './components/signup/Signup.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 className="title">Atologist Infotech</h1>
+      </div>
+      <Signup />
     </div>
   );
 }
